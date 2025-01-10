ae56a5b15363a3eb2c6f8fbb6235fbb6013c6ad5
Geronti-L <gerontiliparteliani@gmail.com>
2025-01-10 18:59:14 -0500

    adding steps and pom extensions

--- src/test/java/stepDefs/Steps.java
+++ src/test/java/stepDefs/Steps.java
@@ -3,19 +3,35 @@ package stepDefs;
 import hooks.DriverManager;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.jupiter.api.Assertions;
+
+
+import static hooks.DriverManager.getDriver;
 
 public class Steps {
 
         @Given("user navigates to Google")
         public void userNavigatesToGoogle() {
-            DriverManager.getDriver().get("https://www.google.com");
+            getDriver().get("https://www.google.com");
             System.out.println("Navigated to Google");
         }
 
         @Then("user checks the page title")
         public void userChecksThePageTitle() {
-            String title = DriverManager.getDriver().getTitle();
+            String title = getDriver().getTitle();
             System.out.println("Page title: " + title);
         }
+
+    @Given("user navigates to {string}")
+    public void userNavigatesTo(String url) {
+            getDriver().get("https://"+url);
     }
+
+    @Then("user checks the page title is {string}")
+    public void userChecksThePageTitleIs(String title) {
+          String actualTitle =  getDriver().getTitle().toString();
+          Assertions.assertEquals(title,actualTitle);
+
+    }
+}
 
--- src/test/resources/features/home.feature
+++ src/test/resources/features/home.feature
@@ -3,3 +3,8 @@ Feature: Google Search
   Scenario: Open Google and verify title
     Given user navigates to Google
     Then user checks the page title
+
+
+    Scenario: Open a page and verify title
+      Given user navigates to "skryabin.com/webdriver/html/basic_form.html"
+      Then user checks the page title is "Basic form"
