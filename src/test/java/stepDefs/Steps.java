fb31f8e0a7dd859ffedbd4e09cee9e832b57c49c
Geronti-L <gerontiliparteliani@gmail.com>
2025-01-10 22:10:12 -0500

    full form fill out

--- src/test/java/stepDefs/Steps.java
+++ src/test/java/stepDefs/Steps.java
@@ -4,7 +4,14 @@ import hooks.DriverManager;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+
+import java.time.Duration;
 
 import static hooks.DriverManager.getDriver;
 
@@ -31,6 +38,52 @@ public class Steps {
     public void userChecksThePageTitleIs(String title) {
           String actualTitle =  getDriver().getTitle().toString();
           Assertions.assertEquals(title,actualTitle);
+
+    }
+
+    @Then("user fill out the form with all required info")
+    public void userFillOutTheFormWithAllRequiredInfo() {
+//
+            getDriver().findElement(By.xpath("//input[@id='name']")).click();
+            getDriver().findElement(By.xpath("//input[@id='firstName']")).sendKeys("john");
+            getDriver().findElement(By.xpath("//input[@id='lastName']")).sendKeys("doe");
+            getDriver().findElement(By.xpath("//span[contains(text(),'Save')]")).click();
+            getDriver().findElement(By.xpath("//input[@ng-model='formData.username']")).sendKeys("username");
+            getDriver().findElement(By.xpath("//input[@ng-model='formData.password']")).sendKeys("userPassword");
+            getDriver().findElement(By.xpath("//input[@ng-model='formData.confirmPassword']")).sendKeys("userPassword");
+            getDriver().findElement(By.xpath("//input[@ng-model='formData.email']")).sendKeys("user@test.com");
+            getDriver().findElement(By.xpath("//input[@ng-model='formData.phone']")).sendKeys("123456789");
+            getDriver().findElement(By.xpath("//input[@ng-model='formData.gender']")).click();
+            getDriver().findElement(By.xpath("//input[@ng-model='formData.dateOfBirth']")).click();
+            getDriver().findElement(By.xpath("//option[contains(text(),'Dec')]")).click();
+            getDriver().findElement(By.xpath("//option[contains(text(),'1983')]")).click();
+            getDriver().findElement(By.xpath("//a[normalize-space()='3']")).click();
+            getDriver().findElement(By.xpath("//input[@ng-model='formData.agreedToPrivacyPolicy']")).click();
+            getDriver().findElement(By.xpath("//select[@name='countryOfOrigin']")).click();
+            getDriver().findElement(By.xpath("//option[contains(text(),'Germany')]")).click();
+            getDriver().findElement(By.xpath("//button[@id='formSubmit']")).click();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    @Then("user waits {int} seconds")
+    public void userWaitsSeconds(int sec) throws InterruptedException {
+            Thread.sleep(sec*1000);
+
 
     }
 }
--- src/test/resources/features/home.feature
+++ src/test/resources/features/home.feature
@@ -11,3 +11,10 @@ Feature: Google Search
       Then user checks the page title is "Basic form"
 
 
+@formfillout
+  Scenario: Fill out the form
+    Given user navigates to "skryabin.com/webdriver/html/quote.html"
+    Then user fill out the form with all required info
+    Then user waits 3 seconds
+
+
